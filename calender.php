419233f21b1badf35b87ee849f4ec40026617188
DhariniCool <jjdharini@gmail.com>
2012-06-03 22:56:45 +0530

    Updated calender.php

--- calender.php
+++ calender.php
@@ -1,97 +1,126 @@
-		<div class="cal">
-			<div style="text-align:center;">
-				<span style="float:left">
-					<img src="images/leftarrow.png" width="30px" height="30px">
-				</span>
-				<span id="cal_month">
-					January 2012
-				</span>
-				<span style="float:right;">
-					<img src="images/rightarrow.png" width="30px" height="30px">
-				</span>
+<html>
+<head>
+		<title>Calender</title>
+		<script  type='text/javascript'>
+			function popup(url) 
+			{
+				window.open( url, 'myWindow', 'status=1, height = 200, width=300, resizable = 1,fullscreen=0' )	
+			}
+		</script>
+</head>
+<body>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<?php
+				
+		$calendar = getCalendar($month,$year);
+		function getCalendar($month = "" , $year = "")
+		{
+			$con=mysql_connect("localhost","root","");		//Coonect to mysql
+			if(!$con)
+			{
+				die('Could not connect: ' . mysql_error());
+			}
+			mysql_select_db("kennisplay_cal",$con);		//select database
+			$year=date("Y");		/*Current year as 2012*/
+			$month=date("n");		/*Current month as 5*/	
+			$day=date("j");		/*Current day as 1 (mon)*/		
+			if (isset($_GET['m'])) 		//get variable from url 
+			{
+				$month = mysql_escape_string(htmlentities(strip_tags($_GET['m'])));
+			}
+			if (isset($_GET['y'])) 
+			{
+				$year = mysql_escape_string(htmlentities(strip_tags($_GET['y'])));
+			}	
+			$last_year = $year;
+			$last_month = $month;
+			$last_month--;
+			if ("$last_month"=="0") 		//if month goes before jan, make it dec of previous year
+			{
+				$last_year--;
+				$last_month = "12";
+			}
+
+			$next_year = $year;
+			$next_month = $month;
+			$next_month++;
+			if ("$next_month"=="13")		//if month goes beyond dec, make it jan of next year
+			 {
+				$next_year++;
+				$next_month = "1";
+			}
+
+			$no_of_days=date("t",mktime(0,0,0,$month,1,$year));    //no of days in a month
+			$firstday=date("w",mktime(0,0,0,$month,1,$year));         /*Day on which first day falls*/
+			$no_of_cells=$firstday + $no_of_days;		
+			$no_of_weeks=ceil($no_of_cells/7);
+			for($j=0;$j<$no_of_weeks;$j++)
+			{
+				for($i=0;$i<7;$i++)
+				{
+					$counter++;
+					$week[$j][$i]=$counter;
+					$week[$j][$i]-=$firstday;
+					if(($week[$j][$i] < 1) || ($week[$j][$i] > $no_of_days))         
+					{	
+						$week[$j][$i] ="";		//make those cells before the first day and after last as empty
+					}			
+				}
+			}
+	
+			echo "
+			<table border='1' cellpadding='2' cellspacing='3'>
+			<div style='text-align:center;'>
+			<tr>
+			<td colspan='1'><span style='float:left;'>
+				<a href='$global[envself]?y=$last_year&m=$last_month' > <img src='images/leftarrow.png' height=30 width=30> </a><span>
+			</td>
+			<th colspan='5' background='calpad.jpg' > <span style='font-size:25px'>	
+			";
+			echo date('M', mktime(0,0,0,$month,1,$year)).' '.$year;  	//display current month and year
+			echo "</span> </th>
+	 		<td colspan='1'><span style='float:right;'>
+			<a href='$global[envself]?y=$next_year&m=$next_month' ><img src='images/rightarrow.png' height=30 width=30> </a></span>
+			</td>
+				</tr>
 			</div>
-			<table cellspacing="0">
-			<thead>
 				<tr>
-					<th>Mon</th><th>Tue</th><th>Wed</th>
-					<th>Thu</th><th>Fri</th><th>Sat</th>
-					<th>Sun</th>
+				<td align='center' background='calpad.jpg'> <b> Sun</b> </td>
+				<td align='center' background='calpad.jpg'> <b>Mon</b></td>
+				<td align='center' background='calpad.jpg'> <b>Tues</b></td>
+				<td align='center' background='calpad.jpg'> <b>Wed</b></td>
+				<td align='center' background='calpad.jpg'> <b>Thur</b></td>
+				<td align='center' background='calpad.jpg'> <b>Fri. </b></td>
+				<td align='center' background='calpad.jpg'> <b>Sat </b></td>
 				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td class="padding"> &nbsp;</td>	
-					<td class="padding"> &nbsp;</td>
-					<td class="padding"> &nbsp;</td>
-					<td> 1</td>
-					<td> 2</td>
-					<td> 3</td>
-					<td> 4</td>
-				</tr>
-				<tr>
-					<td> 5</td>
-					<td> 6</td>
-					<td> 7</td>
-					<td> 8</td>
-					<td class="today"> 9</td>
-					<td>10</td>
-					<td>11</td>
-				</tr>
-				<tr>
-					<td>12</td>
-					<td class="date_has_event">
-						13
-						<div class="events">
-							<ul>
-								<li>
-									<span class="title">Kurukshetra</span>
-									<span class="desc">Technical Fest organised by CEG,Guindy </span>
-								</li>
-								<li>
-									<span class="title">Samhita</span>
-									<span class="desc">Technical Fest conducted by Department of IT, MIT</span>
-								</li>
-							</ul>
-						</div>
-					</td>
-					<td>14</td>
-					<td>15</td>
-					<td>16</td>
-					<td>17</td>
-					<td>18</td>
-				</tr>
-				<tr>
-					<td>19</td>
-					<td>20</td>
-					<td>21</td>
-					<td class="date_has_event">
-						22
-						<div class="events">
-							<ul>
-								<li>
-									<span class="title">Carte Blanche</span>
-									<span class="desc">Free Software Fest organised by Computer Society of MIT</span>
-								</li>
-								<li>
-									<span class="title">Prayatna</span>
-									<span class="desc">Technical Fest conducted by Department of CT, MIT</span>
-								</li>
-							</ul>
-						</div>
-					</td>
-					<td>23</td>
-					<td>24</td>
-					<td>25</td>
-				</tr>	
-				<tr>
-					<td>26</td>
-					<td>27</td>
-					<td>28</td>
-					<td>29</td>
-					<td>30</td>
-					<td>31</td>
-					<td class="padding"></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+			";
+			for($j=0;$j<$no_of_weeks;$j++)
+				{
+					
+					echo "<tr>";
+					for($i=0;$i<7;$i++)
+					{	
+						$url="http://localhost/details.php?yr=".$year."&dat=".$week[$j][$i]."&mn=".$month;		
+						$rs1 = mysql_query("SELECT * FROM calender where month=$month and year=$year");
+						if(($ro = mysql_fetch_assoc($rs1))&&($week[$j][$i]==$ro['day']))	//check if current date has any details in database
+						{
+							echo "<td align='center' background='images/background.jpg'> ";	//if so, make background diffrent
+							echo "<span onClick=popup('$url') > ";
+							echo $week[$j][$i];		
+							echo " </span>";
+						}
+						else
+						{
+							echo "<td align='center' background='images/calpad.jpg'> ";
+							echo $week[$j][$i];		
+						}
+						
+					}
+					echo "</tr> ";
+				}
+			echo "</table>";
+		}
+?>
+<div id="calendar_wrapper"><?PHP echo @$calendar ?></div>		<!-- call calender function -->
+</body>
+</html>
